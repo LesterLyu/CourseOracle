57a31236a6150a99b8e7a6d565bf35974030eb95
LesterLyu <lvds2000@gmail.com>
2021-07-16 16:02:26 -0400

    Install Material UI.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
