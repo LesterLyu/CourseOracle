3f271dd558d03fd13d6681694488dd146adff4e0
luolixuan <lixuan.luo@mail.utoronto.ca>
2021-08-11 20:38:20 -0400

    home page and logos

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,6 +5,19 @@ import {Container, InputBase, Paper} from "@material-ui/core";
 import {Search as SearchIcon} from "@material-ui/icons";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import Favorite from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import {useState} from 'react';
+import Button from '@material-ui/core/Button';
+import ArrowForwardSharpIcon from '@material-ui/icons/ArrowForwardSharp';
+import Icon from '@material-ui/core/Icon';
+
+import {Grid} from '@material-ui/core';
 
 
 const Search = styled('div')(({theme}) => ({
@@ -27,8 +40,7 @@ const SearchIconWrapper = styled('div')(({theme}) => ({
   height: '100%',
   position: 'absolute',
   pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
+  display: 'flex', alignItems: 'center',
   justifyContent: 'center',
 }));
 
@@ -43,34 +55,155 @@ const StyledInputBase = styled(InputBase)(({theme}) => ({
     [theme.breakpoints.up('sm')]: {
       width: '50ch',
       '&:focus': {
-        width: '50ch',
+        width: '52ch',
       },
     },
   },
 }));
 
+// const FormGroup2 = styled('div')(({theme}) => ({
+//   position: 'cnter',
+//   borderRadius: theme.shape.borderRadius,
+
+//   marginLeft: 0,
+//   width: '100%',
+// }));
 
 export default function HomePage() {
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+    checkedC: true,
+  });
+
+  const handleChange = (event) => {
+    setState({...state, [event.target.name]: event.target.checked});
+  };
+
   return (
-    <Container>
+    <Container justify="center">
+
       <Box sx={{display: 'flex'}}>
-        <Typography variant={"h1"} fontSize={40} sx={{pt: 5}}>
-          Homepage Placeholder
+        <Typography variant={"h1"} fontStyle={'sans-serif'} fontSize={40} sx={{pt: 5}}>
+          Course Oracle
         </Typography>
       </Box>
-      <Box sx={{display: 'flex'}}>
-        <Paper sx={{margin: 'auto', marginTop: '50px'}}>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon/>
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-            />
-          </Search>
-        </Paper>
-      </Box>
 
+
+      <Grid container spacing={4} sx={{marginTop: '50px'}} justifyContent="center">
+        <Grid direction={'column'}>
+          <Grid item xs={6} md={8}>
+            <Paper>
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon/>
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search for course…"
+                />
+              </Search>
+            </Paper>
+          </Grid>
+
+          <Grid item>
+            <Box sx={{margin: 'auto'}}>
+              <FormGroup row sx={{justifyContent: 'center'}}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={state.checkedA}
+                      onChange={handleChange}
+                      name="checkedA"
+                      color="primary"
+                    />
+                  }
+                  label="Course Materials"
+                />
+
+
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={state.checkedB}
+                      onChange={handleChange}
+                      name="checkedB"
+                      color="primary"
+                    />
+                  }
+                  label="Course Past Exams"
+                />
+
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={state.checkedC}
+                      onChange={handleChange}
+                      name="checkedC"
+                      color="primary"
+                    />
+                  }
+                  label="Course Ratings"
+                />
+              </FormGroup>
+            </Box>
+          </Grid>
+
+        </Grid>
+
+        <Grid item xs={6} md={2} sx={{mt: '-28px'}}>
+          <Box sx={{display: 'flex'}}>
+            <Button
+              size="small"
+              variant="contained"
+              color="primary"
+              endIcon={<ArrowForwardSharpIcon/>}
+            >
+              Search
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+
+      {/*<Box sx={{margin: 'auto'}}>*/}
+      {/*  <FormGroup row sx={{justifyContent: 'center'}}>*/}
+      {/*    <FormControlLabel*/}
+      {/*      control={*/}
+      {/*        <Checkbox*/}
+      {/*          checked={state.checkedA}*/}
+      {/*          onChange={handleChange}*/}
+      {/*          name="checkedA"*/}
+      {/*          color="primary"*/}
+      {/*        />*/}
+      {/*      }*/}
+      {/*      label="Course Materials"*/}
+      {/*    />*/}
+
+
+      {/*    <FormControlLabel*/}
+      {/*      control={*/}
+      {/*        <Checkbox*/}
+      {/*          checked={state.checkedB}*/}
+      {/*          onChange={handleChange}*/}
+      {/*          name="checkedB"*/}
+      {/*          color="primary"*/}
+      {/*        />*/}
+      {/*      }*/}
+      {/*      label="Course Past Exams"*/}
+      {/*    />*/}
+
+      {/*    <FormControlLabel*/}
+      {/*      control={*/}
+      {/*        <Checkbox*/}
+      {/*          checked={state.checkedC}*/}
+      {/*          onChange={handleChange}*/}
+      {/*          name="checkedC"*/}
+      {/*          color="primary"*/}
+      {/*        />*/}
+      {/*      }*/}
+      {/*      label="Course Ratings"*/}
+      {/*    />*/}
+      {/*  </FormGroup>*/}
+      {/*</Box>*/}
     </Container>
   )
 }
