470b3606b7fb32b682c6adf6828ca94895b095fa
Kevintjy <jingyao.tang99@gmail.com>
2021-08-24 00:47:38 +0800

    change frontend route

--- frontend/src/routes/MaterialRoute/config.js
+++ frontend/src/routes/MaterialRoute/config.js
@@ -1,11 +1,5 @@
 import CourseMaterial from '../../pages/Materials/CourseMaterial';
-import Checkout from '../../pages/Materials/Checkout';
-import RateMaterial from '../../pages/Materials/RateMaterial';
-import Home from '../../pages/Home';
 
 export const config = [
-  {path: '/material/:course', exact: true, Component: CourseMaterial},
-  // TODO: delete following routes in prod env
-  {path: '/material/checkout', exact: true, Component: Checkout},
-  {path: '/material/rate', exact: true, Component: RateMaterial},
+  {path: '/materials', exact: true, Component: CourseMaterial},
 ]
